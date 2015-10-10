65a8d049a21b471c0e54725b743195917112e1c3
Denis Raylyan <draylyan@gmail.com>
2015-10-09 22:00:11 -0400

    tabs

--- widgets.lua
+++ widgets.lua
@@ -59,36 +59,37 @@ function widgets.cpu()
 	local cpuwidget = widget({ type = "textbox" })
 	vicious.register(cpuwidget, vicious.widgets.cpu,
 		function (widget, args)
-		  cpuuse = "" 
-		  cores = 8 --set # of cores/cpus
-		  for i = 1,cores do
-		    if args[i+1] >= 0 and args[i+1] < 10 then
-		      coreuse = theme.fg(theme.green, "0" .. args[i+1])   
-		    elseif  args[i+1] >= 40 and args[i+1] < 80 then
-		      coreuse = theme.fg(theme.yellow, args[i+1])
-		    elseif args[i+1] >= 80 and args[i+1] < 100 then
-		      coreuse = theme.fg(theme.red, args[i+1]) 
-		    elseif args[i+1] >= 100 then
-		      coreuse = theme.fg(theme.bred, "**") 
-		    else
-		      coreuse = theme.fg(theme.green, args[i+1])
-		    end
+			cpuuse = "" 
+			cores = 8 --set # of cores/cpus
+			for i = 1,cores do
+				if args[i+1] >= 0 and args[i+1] < 10 then
+		      		coreuse = theme.fg(theme.green, "0" .. args[i+1])   
+		    	elseif  args[i+1] >= 40 and args[i+1] < 80 then
+		      		coreuse = theme.fg(theme.yellow, args[i+1])
+		    	elseif args[i+1] >= 80 and args[i+1] < 100 then
+		      		coreuse = theme.fg(theme.red, args[i+1]) 
+		    	elseif args[i+1] >= 100 then
+		      		coreuse = theme.fg(theme.bred, "**") 
+		    	else
+		      		coreuse = theme.fg(theme.green, args[i+1])
+		    	end
 		    
-		    if i < cores then 
-		    	cpuuse = cpuuse .. coreuse .. theme.fg(theme.grey, "•")
-	      	else 
-	      		cpuuse = cpuuse .. coreuse
-		    end
-		  end
+		    	if i < cores then 
+		    		cpuuse = cpuuse .. coreuse .. theme.fg(theme.grey, "•")
+		      	else 
+		      		cpuuse = cpuuse .. coreuse
+	    		end
+		  	end
 		  
-		  if args[1] >= 40 and args[1] < 80 then
-		    cpuicon.image = image(beautiful.widget_cpu_mid)
-		  elseif args[1] >= 80 then
-		    cpuicon.image = image(beautiful.widget_cpu_hi)
-		  else
-		    cpuicon.image = image(beautiful.widget_cpu_lo)
-		  end
-		  return cpuuse
+			if args[1] >= 40 and args[1] < 80 then
+		    	cpuicon.image = image(beautiful.widget_cpu_mid)
+		  	elseif args[1] >= 80 then
+			    cpuicon.image = image(beautiful.widget_cpu_hi)
+		  	else
+			    cpuicon.image = image(beautiful.widget_cpu_lo)
+		  	end
+
+		  	return cpuuse
 		end, 1 )
 
 	cpu = {
@@ -106,21 +107,21 @@ function widgets.wifi()
 	
 	local wifiwidget = widget({ type = "textbox" }) --display SSID and rate
 	vicious.register(wifiwidget, vicious.widgets.wifi,
-     function (widget, args)
-      if args["{link}"] == 0 then
-        wifiicon.image = image(beautiful.widget_wifi_off)
-        return theme.fg(theme.bred, "∅")
-      elseif args["{rate}"] <= 11 then
-        wifiicon.image = image(beautiful.widget_wifi_lo)
-        return string.format("%s " .. theme.fg(theme.red, "(%i mbps)"), args["{ssid}"], args["{rate}"])
-      elseif args["{rate}"] > 11 and args["{rate}"] < 54 then
-        wifiicon.image = image(beautiful.widget_wifi_mid)
-        return string.format("%s " .. theme.fg(theme.yellow, "(%i mbps)"), args["{ssid}"], args["{rate}"])
-      else
-        wifiicon.image = image(beautiful.widget_wifi_hi)
-        return string.format("%s " .. theme.fg(theme.green, "(%i mbps)"), args["{ssid}"], args["{rate}"])
-      end
-     end, 11, WIFI_ADAPTER_NAME)
+		function (widget, args)
+			if args["{link}"] == 0 then
+	        	wifiicon.image = image(beautiful.widget_wifi_off)
+	        	return theme.fg(theme.bred, "∅")
+	      	elseif args["{rate}"] <= 11 then
+	        	wifiicon.image = image(beautiful.widget_wifi_lo)
+	        	return string.format("%s " .. theme.fg(theme.red, "(%i mbps)"), args["{ssid}"], args["{rate}"])
+	      	elseif args["{rate}"] > 11 and args["{rate}"] < 54 then
+	        	wifiicon.image = image(beautiful.widget_wifi_mid)
+	        	return string.format("%s " .. theme.fg(theme.yellow, "(%i mbps)"), args["{ssid}"], args["{rate}"])
+	      	else
+	        	wifiicon.image = image(beautiful.widget_wifi_hi)
+	        	return string.format("%s " .. theme.fg(theme.green, "(%i mbps)"), args["{ssid}"], args["{rate}"])
+	      	end
+	     end, 11, WIFI_ADAPTER_NAME)
 
 	wifi = {
 		wifiicon,
@@ -178,21 +179,21 @@ function widgets.volume()
 	volicon.image = image(beautiful.widget_vol_hi)
 	local volumewidget = widget({ type = "textbox" }) --display volume
 	vicious.register(volumewidget, vicious.widgets.volume, 
-	function (widget, args)
-	  if args[1] >= 37 and args[1] < 66 then
-	    volicon.image = image(beautiful.widget_vol_mid)
-	    return theme.fg(theme.yellow, args[1])
-	  elseif args[1] >= 66 then
-	    volicon.image = image(beautiful.widget_vol_hi)
-	    return theme.fg(theme.green, args[1])
-	  elseif args[1] == 0 then
-	    volicon.image = image(beautiful.widget_vol_mute)
-	    return theme.fg(theme.red, args[1])
-	  else
-	    volicon.image = image(beautiful.widget_vol_lo)
-	    return theme.fg(theme.red, args[1])
-	  end
-	end, 5, "Master")
+		function (widget, args)
+			if args[1] >= 37 and args[1] < 66 then
+		    	volicon.image = image(beautiful.widget_vol_mid)
+		    	return theme.fg(theme.yellow, args[1])
+		  	elseif args[1] >= 66 then
+			    volicon.image = image(beautiful.widget_vol_hi)
+			    return theme.fg(theme.green, args[1])
+		  	elseif args[1] == 0 then
+		    	volicon.image = image(beautiful.widget_vol_mute)
+		    	return theme.fg(theme.red, args[1])
+		  	else
+			    volicon.image = image(beautiful.widget_vol_lo)
+			    return theme.fg(theme.red, args[1])
+		  	end
+		end, 5, "Master")
 
 	volume = {
 		volumewidget,
@@ -211,24 +212,23 @@ function widgets.battery()
 	batchrg.image = image(beautiful.widget_bat_full)
 	local batterywidget = widget({ type = "textbox" }) --display battery state and charge
 	vicious.register(batterywidget, vicious.widgets.bat,
-	  function (widget, args)
-	    if args[2] >= 30 and args [2] < 75 then
-	      baticon.image = image(beautiful.widget_bat_mid)
-	      return theme.fg(theme.yellow, args[2] .. "%")
-	    elseif args[2] >= 10 and args[2] < 30 then
-	      baticon.image = image(beautiful.widget_bat_lo)
-	      return theme.fg(theme.red, args[2] .. "%")
-	    elseif args[2] >= 6 and args[2] < 10 then
-	      baticon.image = image(beautiful.widget_bat_lo)
-	      return theme.fg(theme.red, args[2] .. "%")
-	    elseif args[2] < 6 then
-	      baticon.image = image(beautiful.widget_bat_crit)
-	    else
-	      baticon.image = image(beautiful.widget_bat_hi)
-	      return theme.fg(theme.green, args[2] .. "%")
-	    end
-
-	  end, 61, BATTERY_NAME)
+		function (widget, args)
+			if args[2] >= 30 and args [2] < 75 then
+		      	baticon.image = image(beautiful.widget_bat_mid)
+	      		return theme.fg(theme.yellow, args[2] .. "%")
+	    	elseif args[2] >= 10 and args[2] < 30 then
+				baticon.image = image(beautiful.widget_bat_lo)
+		      	return theme.fg(theme.red, args[2] .. "%")
+		    elseif args[2] >= 6 and args[2] < 10 then
+		      	baticon.image = image(beautiful.widget_bat_lo)
+		      	return theme.fg(theme.red, args[2] .. "%")
+		    elseif args[2] < 6 then
+		      	baticon.image = image(beautiful.widget_bat_crit)
+		    else
+		      	baticon.image = image(beautiful.widget_bat_hi)
+		      	return theme.fg(theme.green, args[2] .. "%")
+		    end
+		end, 61, BATTERY_NAME)
 
 	battery = {
 		batterywidget,
